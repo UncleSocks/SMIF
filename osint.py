7fd5475e1bba19f494e76c66a81ee707aa39229f
UncleSocks <ilisan.tyrone@gmail.com>
2026-02-09 20:26:41 +0800

    Update: Added user information URLs.

--- osint.py
+++ osint.py
@@ -9,8 +9,13 @@ root = tk.Tk()
 root.title("Social Media Intelligence Filter-inator")
 
 
-
-FACEBOOK_BASE_URL = "https://www.facebook.com/search/"
+FACEBOOK_BASE_URL = "https://www.facebook.com/"
+FACEBOOK_BASE_SEARCH_URL = "https://www.facebook.com/search/"
+UNAME_INFORMATION_MAP = {
+    "timeline":"",
+    "about":"about",
+    "employment":"about?section=work"
+}
 
 
 
@@ -31,17 +36,27 @@ class ConstructFbUrl:
         self.keyword = (quote(keyword_entry.get().lower()) if keyword_entry.get() else self.selected_type)
         self.selected_year = select_year.get().lower()
 
-    def construct_user_url(self, user_id):
+    def construct_user_url(self, user_id=None):
+        
+        if not user_id:
+            output = "Unable to generate URL. Enter a user ID."
+            return output
+
         if self.selected_year == "top":
             raw_filter = f'{{"rp_author":"{{\\"name\\":\\"author\\",\\"args\\":\\"{user_id}\\"}}"}}'
         else:
             raw_filter = f'{{"rp_author":"{{\\"name\\":\\"author\\",\\"args\\":\\"{user_id}\\"}}","rp_creation_time":"{{\\"name\\":\\"creation_time\\",\\"args\\":\\"{{\\\\\\"start_year\\\\\\":\\\\\\"{self.selected_year}\\\\\\",\\\\\\"start_month\\\\\\":\\\\\\"{self.selected_year}-1\\\\\\",\\\\\\"end_year\\\\\\":\\\\\\"{self.selected_year}\\\\\\",\\\\\\"end_month\\\\\\":\\\\\\"{self.selected_year}-12\\\\\\",\\\\\\"start_day\\\\\\":\\\\\\"{self.selected_year}-1-1\\\\\\",\\\\\\"end_day\\\\\\":\\\\\\"{self.selected_year}-12-31\\\\\\"}}\\"}}"}}'
         
         encoded_filter = encode(raw_filter)
-        new_fb_url = FACEBOOK_BASE_URL + f"{self.selected_type}" + f"?q={self.keyword}" + f"&epa=FILTERS&filters={encoded_filter}"
+        new_fb_url = FACEBOOK_BASE_SEARCH_URL + f"{self.selected_type}" + f"?q={self.keyword}" + f"&epa=FILTERS&filters={encoded_filter}"
         return new_fb_url
     
-    def construct_location_url(self, location_id):
+    def construct_location_url(self, location_id=None):
+        
+        if not location_id:
+            output = "Unable to generate URL. Enter a location ID."
+            return output
+
         if self.selected_type == "events":
             raw_filter = f'{{"rp_events_location":"{{\\"name\\":\\"filter_events_location\\",\\"args\\":\\"{location_id}\\"}}"}}'
         
@@ -58,7 +73,7 @@ class ConstructFbUrl:
             raw_filter = f'{{"rp_author":"{{\\"name\\":\\"location\\",\\"args\\":\\"{location_id}\\"}}","rp_creation_time":"{{\\"name\\":\\"creation_time\\",\\"args\\":\\"{{\\\\\\"start_year\\\\\\":\\\\\\"{self.selected_year}\\\\\\",\\\\\\"start_month\\\\\\":\\\\\\"{self.selected_year}-1\\\\\\",\\\\\\"end_year\\\\\\":\\\\\\"{self.selected_year}\\\\\\",\\\\\\"end_month\\\\\\":\\\\\\"{self.selected_year}-12\\\\\\",\\\\\\"start_day\\\\\\":\\\\\\"{self.selected_year}-1-1\\\\\\",\\\\\\"end_day\\\\\\":\\\\\\"{self.selected_year}-12-31\\\\\\"}}\\"}}"}}'
 
         encoded_filter = encode(raw_filter)
-        new_fb_url = FACEBOOK_BASE_URL + f"{self.selected_type}" + f"?q={self.keyword}" + f"&epa=FILTERS&filters={encoded_filter}"
+        new_fb_url = FACEBOOK_BASE_SEARCH_URL + f"{self.selected_type}" + f"?q={self.keyword}" + f"&epa=FILTERS&filters={encoded_filter}"
         return new_fb_url
     
     def construct_profile_url(self, id, profile_id, profile):
@@ -70,11 +85,17 @@ class ConstructFbUrl:
         
         raw_filter = f'{{"{profile_map[id]["filter"]}":"{{\\"name\\":\\"{profile_map[id]["name"]}\\",\\"args\\":\\"{profile_id}\\"}}"}}'
         encoded_filter = encode(raw_filter)
-        new_fb_url = FACEBOOK_BASE_URL + f"people/?q={profile}" + f"&epa=FILTERS&filters={encoded_filter}"
+        new_fb_url = FACEBOOK_BASE_SEARCH_URL + f"people/?q={profile}" + f"&epa=FILTERS&filters={encoded_filter}"
         return new_fb_url
+    
+    def construct_username_url(self, username, information):
+        new_fb_url = FACEBOOK_BASE_URL + f"{username}/" + f"{UNAME_INFORMATION_MAP[information]}"
+        return new_fb_url
+        
 
     def construct_url(self):
         id  = id_types.get().lower()
+        username = username_entry.get().lower()
         if id == "user id":
             user_id = id_entry.get()
             self._capture_keyword_and_year()
@@ -89,9 +110,16 @@ class ConstructFbUrl:
         
         elif id == "employer id" or id == "city id" or id == "school id":
             profile_id = id_entry.get()
-            profile = profile_entry.get()
+            profile = quote(profile_entry.get())
             new_fb_url = self.construct_profile_url(id, profile_id, profile)
             return new_fb_url
+        
+        elif username:
+            information = uname_information.get().lower()
+            new_fb_url = self.construct_username_url(username, information)
+            return new_fb_url
+            
+
 
 
 
@@ -118,9 +146,17 @@ def display_ids(event=None):
         or selected_type == "videos":
         id_types.config(value=["User ID", "Location ID"], state="readonly")
 
-        select_year.config(values=["Top", "2025", "2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010"],
-                           state="readonly")
+        select_year.config(state="readonly")
         select_year.set("Top")
+
+        profile_entry.delete(0, tk.END)
+        profile_entry.config(state="disabled")
+        
+        username_entry.delete(0, tk.END)
+        username_entry.config(state="disabled")
+        
+        uname_information.delete(0, tk.END)
+        uname_information.config(state="disabled")
 
     elif selected_type == "profiles":
         id_types.config(values=["Employer ID", "City ID", "School ID"], state="readonly")
@@ -132,6 +168,16 @@ def display_ids(event=None):
 
     elif selected_type == "events":
         id_types.config(values=["Location ID"], state="readonly")
+        select_year.set("")
+        select_year.config(state="disabled")
+
+    elif selected_type == "user info":
+        username_entry.config(state="normal")
+        uname_information.config(state="readonly")
+        id_entry.delete(0, tk.END)
+        id_entry.config(state="disabled")
+        keyword_entry.delete(0, tk.END)
+        keyword_entry.config(state="disabled")
         select_year.set("")
         select_year.config(state="disabled")
 
@@ -162,7 +208,7 @@ search_label = tk.Label(root, text="Search Type:")
 search_label.grid(row=0, column=0, padx=10, pady=10)
 
 search_types = ttk.Combobox(root,
-                            values=["Posts", "Photos", "Videos", "Profiles", "Events", "Account Details"],
+                            values=["Posts", "Photos", "Videos", "Profiles", "Events", "User Info", "Search"],
                             state="readonly")
 search_types.grid(row=0, column=1, padx=10)
 search_types.bind("<<ComboboxSelected>>", display_ids)
@@ -191,12 +237,33 @@ profile_entry.grid(row=2, column=3)
 
 year_label = tk.Label(root, text="Year:")
 year_label.grid(row=2, column=4, padx=10)
-select_year = ttk.Combobox(root, state="disabled")
+select_year = ttk.Combobox(root, values=["Top", "2025", "2024", "2023", "2022", "2021", "2020", "2019", "2018", "2017", "2016", "2015", "2014", "2013", "2012", "2011", "2010"],state="disabled")
 select_year.grid(row=2, column=5)
+
+username_label = tk.Label(root, text="Username:")
+username_label.grid(row=3, column=0, padx=10, pady=15)
+username_entry = tk.Entry(root, state="disabled")
+username_entry.grid(row=3, column=1)
+
+uname_information_label = tk.Label(root, text="Information:")
+uname_information_label.grid(row=3, column=2, padx=10)
+uname_information = ttk.Combobox(root,
+                                 values=["Timeline", "About", "Employment", 
+                                         "Education", "Locations", "Contact Info",
+                                         "Basic Info", "Relationships", "Family",
+                                         "Biography", "Life Events", "Friends",
+                                         "Following", "Photos", "Photos Albums",
+                                         "Videos", "Reels", "Check-ins", "Visits",
+                                         "Recent Check-ins", "Sports", "Music",
+                                         "Movies", "TV", "Books", "Apps & Games",
+                                         "Likes", "Events", "Facts", "Reviews",
+                                         "Reviews Given", "Reviews Written", "Notes"],
+                                         state="disabled")
+uname_information.grid(row=3, column=3)
 
 
 output_frame = tk.Frame(root)
-output_frame.grid(row=4, column=0, columnspan=4, sticky="w", padx=10)
+output_frame.grid(row=5, column=0, columnspan=4, sticky="w", padx=10)
 output_label = tk.Label(output_frame, wraplength=500, justify="left", anchor="w")
 output_label.pack(fill="x")
 
@@ -208,7 +275,7 @@ get_ids()
 generate_button = tk.Button(root,
                             text="Generate URL",
                             command=generate_url)
-generate_button.grid(row=3, column=0, padx=20, pady=25)
+generate_button.grid(row=4, column=0, padx=20, pady=25)
 
 
 
